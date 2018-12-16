1058acde951ffb029d37a1ba203f703c9973fbb7
KevinJASD <kev.anderson16@yahoo.com>
2018-12-16 12:25:29 -0500

    working 3/6 variable state tests

--- kalman/kalman-tests/kalman6.cpp
+++ kalman/kalman-tests/kalman6.cpp
@@ -5,60 +5,69 @@
 
 #include "../kalman_.hpp"
 
-//using namespace std;
 using namespace Eigen;
-
-// !**-->  All matricies have fixed size, eventually should change them to fixed size for Eigen!!!!!
 
 // State is defined as position, velocity and acceleration on 3D axis
 /*
- *    | x''   |
- *    | y''   |
- *    | z''   |
+ *    | x'   |
+ *    | y'   |
+ *    | z'   |
+ *    | x''  |
+ *    | y''  |
+ *    | z''  |
  */
 
 int main() {
-
    // Create file stream
-   std::fstream file("../testing output files/kalman.csv", std::fstream::out);
    std::fstream gain("../testing output files/gain.dat", std::fstream::out);
+   std::fstream file("../testing output files/kalman.csv", std::fstream::out);
    // Read in values from file
-   std::fstream data("../../embedded/trials/stationary.csv", std::fstream::in);
+   std::fstream data("../../embedded/trials/straight_return.csv", std::fstream::in);
    //std::fstream data("stationary_filtered.csv", std::fstream::in);
    // Time step (set by the accelerometer)
    float fs = 3200;
    float step = 1/fs;
    // Initial conditions (need initialized values)
-   VectorXd x_init(3);
-   MatrixXd P_init(3,3);
+   VectorXd x_init(6);
+   MatrixXd P_init(6,6);
    // Transformation matricies
-   MatrixXd F(3,3);   // Transformation matrix for prediction (F)
+   MatrixXd F(6,6);   // Transformation matrix for prediction (F)
    //MatrixXd B;   // Uncertainty matrix
-   MatrixXd H(3,3);   // Transformation matrix for readings (C)
+   MatrixXd H(3,6);   // Transformation matrix for readings (C)
    // Covariance matricies
-   MatrixXd Q(3,3);
+   MatrixXd Q(6,6);
    MatrixXd R(3,3);
    // Initialize matricies
-   x_init << 0, 0, 0;          // Assume intial position is zero
-   P_init << 0, 0, 0,           // Assume intial velocity is zero
-             0, 0, 0,
-             0, 0, 0;
-   F << 1, 0, 0,           // Assume intial velocity is zero
-        0, 1, 0,
-        0, 0, 1;
-   H << 1, 0, 0,
-        0, 1, 0,
-        0, 0, 1;
-   Q << 5, 0, 0,
-        0, 5, 0,
-        0, 0, 5;
+   x_init << 0, 0, 0, 0, 0, 0;     // Assume intial position is zero
+   P_init << 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0;
+   F << 1, 0, 0, step, 0, 0,          // Assume intial velocity is zero
+        0, 1, 0, 0, step, 0,
+        0, 0, 1, 0, 0, step,
+        0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 1;
+
+   H << 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 1;
+   Q << 10, 0, 0, 0, 0, 0,
+        0, 10, 0, 0, 0, 0,
+        0, 0, 10, 0, 0, 0,
+        0, 0, 0, 10, 0, 0,
+        0, 0, 0, 0, 10, 0,
+        0, 0, 0, 0, 0, 10;
                       // In reality need to get value from some steady state
    R << 8, 0, 0,
         0, 8, 0,
         0, 0, 8;
 
    // Kalman filter
-   KalmanFilter kalman(0, F, H, Q, R, P_init);
+   KalmanFilter kalman(step, F, H, Q, R, P_init);
    kalman.init(0, x_init);    // Initialize state at t = 0
 
    // For testing, launch process to monitor key strokes (A - means stop
@@ -69,6 +78,8 @@ int main() {
    VectorXd x_(3);           // Temporary container for x,y,z acceleration
    bool stop = false;
 
+   int n = 6;
+   int m = 3;
    int iter = 0;                // for testing; tracks iterations
    int test_len = 30000;        // for testing; max number of iterations
    char * buff = new char[10];  // for testing;
@@ -76,14 +87,13 @@ int main() {
    char filtered;
    std::cout << "is file filtered (y/n): ";
    std::cin >> filtered;
-
-   // Ignore x,y,z line
-   if(filtered == 'n') {
-     data.ignore(10, '\n');
-   }
    std::cout << "iterations: ";
    std::cin >> test_len;
 
+   // Ignore x,y,z line
+   if(filtered = 'n') {
+     data.ignore(10, '\n');
+   }
    MatrixXd K;
 
    // Stop when 'a' key pressed
@@ -97,22 +107,25 @@ int main() {
      data.getline(buff, 10, '\n');
      z(2) = atof(buff);
 
-     kalman.update(z);
-     x_ = kalman.state();
+    kalman.update(z);
+    x_ = kalman.state();
 
     K = kalman.gain();
-    for(int r = 0; r < 3; r++) {
-      for(int c = 0; c < 3; c++) {
-        if(c == 2 && r == 2) {
+    for(int r = 0; r < n; r++) {
+      for(int c = 0; c < m; c++) {
+        if(c == m-1) {
            gain << K(r,c) << std::endl;
+           if(r == n-1) {
+             gain <<std::endl;
+           }
         } else {
            gain << K(r,c) << ", ";
        }
       }
     }
 
-    for(int i = 0; i < 3; i++){
-     if(i == 2) {
+    for(int i = 0; i < n; i++){
+     if(i == n-1) {
        file << x_(i) << std::endl;
      } else {
        file << x_(i) << ", ";
@@ -128,5 +141,6 @@ int main() {
    data.close();
    file.close();
    gain.close();
+
    return 0;
 }
